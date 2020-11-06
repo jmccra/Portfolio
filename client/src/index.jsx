3f959b5a652097743df89d63b67febcd643a609c
Julian <mccrary.julian@gmail.com>
2020-11-06 16:07:37 -0700

    Portfolio initialize

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+function Index(){
+    return(
+      <>
+      Hello
+      </>
+    )
+}
+
+
+
+ReactDOM.render(<Index />, document.getElementById('root'));
